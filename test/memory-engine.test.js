c525ab9e83192efb015d61ff1ec144e50b4e01d1
Josh Wilson <Josh.wilson@clock.co.uk>
2013-02-15 09:57:53 +0000

    Moving memory-engine tests to right file

--- test/memory-engine.test.js
+++ test/memory-engine.test.js
@@ -3,3 +3,18 @@ function getEngine(callback) {
 }
 
 require('./engine.tests')('_id', getEngine)
+
+describe('#data', function () {
+
+  it('should return a empty object if no options set', function () {
+    var engine = require('../lib/memory-engine')()
+    engine.getData().should.eql({})
+  })
+
+  it('should return object when data options are passed', function () {
+    var data = { a: 1 }
+      , engine = require('../lib/memory-engine')({ data: data })
+    engine.getData().should.eql({ a: 1 })
+  })
+
+})
