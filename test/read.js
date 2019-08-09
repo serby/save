b4ac1b68d606c2a23e16faff9477f9e2d9bd64fa
MantasMikal <mantas@Mantass-MacBook-Air.local>
2019-08-09 12:55:21 +0100

    Prettify

--- test/read.js
+++ test/read.js
@@ -1,38 +1,32 @@
 var assert = require('assert')
 
 module.exports = function(idProperty, getEngine) {
-
-  describe('#read()', function () {
-
-    it('should return undefined if no object is found with given id', function (done) {
-
-      getEngine(function (error, engine) {
-        engine.read('999', function (error, entity) {
+  describe('#read()', function() {
+    it('should return undefined if no object is found with given id', function(done) {
+      getEngine(function(error, engine) {
+        engine.read('999', function(error, entity) {
           assert.equal(undefined, entity)
           done()
         })
       })
     })
 
-    it('should emit a \'read\' event', function (done) {
-      getEngine(function (error, engine) {
-        engine.on('read', function (id) {
+    it("should emit a 'read' event", function(done) {
+      getEngine(function(error, engine) {
+        engine.on('read', function(id) {
           assert.equal(id, '999')
           done()
         })
-        engine.read('999', function () {
-        })
+        engine.read('999', function() {})
       })
     })
 
-    it('should return object if id is found', function (done) {
-
+    it('should return object if id is found', function(done) {
       var original = { a: 1 }
 
-      getEngine(function (error, engine) {
-        engine.create(original, function (error, entity) {
-
-          engine.read(entity[idProperty], function (error, entity) {
+      getEngine(function(error, engine) {
+        engine.create(original, function(error, entity) {
+          engine.read(entity[idProperty], function(error, entity) {
             assert.equal(entity, entity)
             done()
           })
@@ -40,32 +34,28 @@ module.exports = function(idProperty, getEngine) {
       })
     })
 
-    it('should emit a received event', function (done) {
-
+    it('should emit a received event', function(done) {
       var original = { a: 1 }
 
-      getEngine(function (error, engine) {
-        engine.create(original, function (error, entity) {
-          engine.on('received', function (data) {
+      getEngine(function(error, engine) {
+        engine.create(original, function(error, entity) {
+          engine.on('received', function(data) {
             assert.deepEqual(data, entity)
             done()
           })
-          engine.read(entity[idProperty], function (error, entity) {
+          engine.read(entity[idProperty], function(error, entity) {
             assert.deepEqual(entity, entity)
           })
         })
       })
     })
 
-    it('should return id of type String', function (done) {
-
+    it('should return id of type String', function(done) {
       var original = { a: 1 }
 
-      getEngine(function (error, engine) {
-        engine.create(original, function (error, entity) {
-
-          engine.read(entity[idProperty], function (error, entity) {
-
+      getEngine(function(error, engine) {
+        engine.create(original, function(error, entity) {
+          engine.read(entity[idProperty], function(error, entity) {
             entity[idProperty].should.be.type('string')
 
             done()
@@ -74,14 +64,12 @@ module.exports = function(idProperty, getEngine) {
       })
     })
 
-    it('should return a clone of the object', function (done) {
+    it('should return a clone of the object', function(done) {
       var original = { a: 1 }
 
-      getEngine(function (error, engine) {
-        engine.create(original, function (error, entity) {
-
-          engine.read(entity[idProperty], function (error, entity) {
-
+      getEngine(function(error, engine) {
+        engine.create(original, function(error, entity) {
+          engine.read(entity[idProperty], function(error, entity) {
             entity.newProperty = true
             original.should.not.have.property('newProperty')
 
@@ -90,7 +78,5 @@ module.exports = function(idProperty, getEngine) {
         })
       })
     })
-
   })
-
 }
--- test/save.test.js
+++ test/save.test.js
@@ -1,13 +1,11 @@
-describe('save()', function () {
-  it('should error if name is missing', function () {
-
-    (function () {
+describe('save()', function() {
+  it('should error if name is missing', function() {
+    ;(function() {
       require('..')()
-    }).should.throwError('A string must be provided for \'name\'')
-
+    }.should.throwError("A string must be provided for 'name'"))
   })
-  it('should fire a find event', function (done) {
-    var save = require('..')('jim', { logger: { info: function () {} } })
+  it('should fire a find event', function(done) {
+    var save = require('..')('jim', { logger: { info: function() {} } })
     save.find({}, done)
   })
 })
